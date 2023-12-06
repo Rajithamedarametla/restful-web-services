d98c574f4a3ba03070357a69fce420bcb360afd1
raji <rajithamedarametla31@gmail.com>
2023-12-06 16:02:34 -0600

    connecting user to web

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -12,20 +12,25 @@ public class UserDaoService {
 
     private static List<User> users = new ArrayList<>();
 
-        static {
-            users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
-            users.add(new User(2,"Eve",LocalDate.now().minusYears(25)));
-            users.add(new User(3,"Jim",LocalDate.now().minusYears(20)));
-        }
+    private static int usersCount = 0;
 
-        public List<User> findAll() {
-            return users;
-        }
+    static {
+        users.add(new User(++usersCount,"Adam",LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount,"Eve",LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount,"Jim",LocalDate.now().minusYears(20)));
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
 
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().get();
     }
+
 
 }
 
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -13,16 +11,17 @@ public class UserResource {
     public UserResource(UserDaoService service) {
         this.service = service;
     }
-
-    // GET /users
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
-    // GET /users
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 }
